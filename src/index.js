76e9881e8decaea8114939f1ab31537792a6051a
haile1501 <hai.lehoang1501@gmail.com>
2023-06-09 17:09:49 +0700

    Docker

--- src/index.js
+++ src/index.js
@@ -18,5 +18,5 @@ app.use("/api", require("./routes"));
 const {handleError} = require("./errors/handle-errors-middleware");
 app.use(handleError);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
